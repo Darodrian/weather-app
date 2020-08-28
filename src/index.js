25919504b430bcb6d17404d9b6a08f366032cae2
Darodrian <st.rookeer@gmail.com>
2020-08-27 21:25:51 -0400

    Added: Google Maps component, Maps API key

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Map from "./components/map";
+import credentials from "./credentials";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const mapURL =
+  "https://maps.googleapis.com/maps/api/js?key=" + credentials.mapsKey;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Map
+          googleMapURL={mapURL}
+          containerElement={<div style={{ height: "400px" }} />}
+          mapElement={<div style={{ height: "100%" }} />}
+          loadingElement={<p>Cargando...</p>}
+        />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
