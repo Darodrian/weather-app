9c2c8176a55aec337c14d41ce92181ef86e07924
Darodrian <st.rookeer@gmail.com>
2020-09-02 20:05:10 -0400

    Working on map markers function - Added map style

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,12 +1,54 @@
 import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+} from "react-google-maps";
+import mapStyle from "../../style/mapStyle";
 
 const Map = (props) => {
+  const center = {
+    lat: -33.492,
+    lng: -70.555115,
+  };
+  const options = {
+    styles: mapStyle,
+    disableDefaultUI: true,
+    zoomControl: true,
+  };
+  const [markers, setMarkers] = React.useState([]);
+  const onMapClick = React.useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
   return (
     <GoogleMap
-      defaultZoom={15}
-      defaultCenter={{ lat: -33.492, lng: -70.555115 }}
-    />
+      defaultZoom={12}
+      defaultCenter={center}
+      options={options}
+      onClick={onMapClick}
+      onLoad={onMapLoad}
+    >
+      {markers.map((marker) => (
+        <Marker
+          key={marker.time.toISOString()}
+          position={{ lat: marker.lat, lng: marker.lng }}
+        />
+      ))}
+    </GoogleMap>
   );
 };
 
