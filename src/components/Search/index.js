d149b8d4bcf68f69a50672ae8163907568652a6c
Darodrian <alon.gonzalezs@gmail.com>
2021-03-27 02:01:08 -0300

    WIP: panTo() : Map relocation

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,53 +1,65 @@
 import React from "react";
 import usePlacesAutocomplete, {
   getGeocode,
-  getLatLng
+  getLatLng,
 } from "use-places-autocomplete";
 import {
   Combobox,
   ComboboxInput,
   ComboboxPopover,
-  ComboboxList,
-  ComboboxOption
+  ComboboxOption,
 } from "@reach/combobox";
 import "@reach/combobox/styles.css";
 
-const Search = () => {
-    const {
-        ready, 
-        value, 
-        suggestions: {status, data}, 
-        setValue, 
-        clearSuggestion
-    } = usePlacesAutocomplete({
-        requestOptions: {
-          location: { lat: () => -33.492, lng: () => -70.555115 },
-          radius: 200 * 1000,
-        }
-    });
+const Search = ({ panTo }) => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => -33.492, lng: () => -70.555115 },
+      radius: 200 * 1000,
+    },
+  });
 
-    return (
-        <div className="search">
-            <Combobox 
-                onSelect={(address) => {
-                    console.log(address);
-                }}
-            >
-                <ComboboxInput 
-                    value={value} 
-                    onChange={(e) => {
-                        setValue(e.target.value);
-                    }}
-                    disabled={!ready}
-                    placeholder="Enter an address"
-                    style={{width: '100%', height: '6vh'}}
-                />
-                <ComboboxPopover>
-                    {status === "OK" && data.map(({id, description}) => <ComboboxOption key={id} value={description} />)}
-                </ComboboxPopover>
-            </Combobox>
-        </div>
-    );
-  };
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            console.log(lat, lng);
+            console.log(results[0]);
 
-  export default Search;
+            // WIP: Map Relocation
+            // panTo({ lat, lng });
+          } catch (error) {
+            console.log("Error");
+          }
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Enter an address"
+          style={{ width: "100%", height: "32px" }}
+        />
+        <ComboboxPopover style={{ height: "auto" }}>
+          {status === "OK" &&
+            data.map(({ id, description }) => (
+              <ComboboxOption key={id} value={description} />
+            ))}
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+};
+
+export default Search;
